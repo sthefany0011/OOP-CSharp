a3f920caff4feef9836ee3580cf4cc4940125cb2
Sthefany Silva <sthefany0011@gmail.com>
2024-01-21 11:04:12 -0300

    exercicio de encapsulamento

---
+++ OrientadaAObjetos1/Exercicio1/ExercicioEncapsulamento/Conta.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioEncapsulamento
+{
+    public class Conta
+    {
+
+
+        public int NumeroDaConta { get; private set; }
+        public string Titular { get; set; }
+
+        public double Saldo { get; private set; }
+
+
+
+
+
+        public Conta(int numeroDaConta, string titular)
+        {
+            NumeroDaConta = numeroDaConta;
+            Titular = titular;
+
+        }
+
+        public Conta(int numeroDaConta, string titular, double saldo) : this(numeroDaConta, titular)
+        {
+            Saldo = saldo;
+        }
+
+        public void Deposito(double quantia)
+        {
+            Saldo += quantia;
+        }
+
+        public void Saque(double quantia)
+        {
+            Saldo -= quantia;
+            Saldo -= 5.00;
+        }
+
+
+        public override string ToString()
+        {
+            return "Conta "
+                + NumeroDaConta
+                + ", Titular: "
+                + Titular
+                + ", Saldo: $ "
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
+
---
+++ OrientadaAObjetos1/Exercicio1/ExercicioEncapsulamento/Program.cs
@@ -0,0 +1,42 @@
+﻿
+using ExercicioEncapsulamento;
+using System.Globalization;
+
+Conta conta;
+
+Console.Write("Entre o número da conta: ");
+int numero = int.Parse(Console.ReadLine());
+Console.Write("Entre o titular da conta: ");
+string titular = Console.ReadLine();
+Console.Write("Haverá depósito inicial (s/n)? ");
+char resp =char.Parse(Console.ReadLine());
+
+
+if(resp == 's' || resp == 'S')
+{
+    Console.Write("Entre o valor de depósito inicial: ");
+    double valorDepositoInicial = double.Parse(Console.ReadLine());
+    conta = new Conta(numero, titular, valorDepositoInicial);
+}
+
+else
+{
+    conta = new Conta(numero, titular);
+}
+Console.WriteLine();
+Console.WriteLine("Dados da conta:");
+Console.WriteLine(conta);
+
+Console.WriteLine();
+Console.Write("Entre um valor para depósito: ");
+double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+conta.Deposito(quantia);
+Console.WriteLine("Dados da conta atualizados:");
+Console.WriteLine(conta);
+
+Console.WriteLine();
+Console.Write("Entre um valor para saque: ");
+quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+conta.Saque(quantia);
+Console.WriteLine("Dados da conta atualizados:");
+Console.WriteLine(conta);
