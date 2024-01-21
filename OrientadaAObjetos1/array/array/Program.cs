dba5e827cdc3065cfaf57b4496abc6fef286a6f2
Sthefany Silva <sthefany0011@gmail.com>
2024-01-21 15:17:45 -0300

    exercicio de array

---
+++ OrientadaAObjetos1/array/array/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using array;
+
+Rend[] vect= new Rend[10];
+
+Console.WriteLine("Quantos quartos serão alugados? ");
+
+int n= int.Parse(Console.ReadLine());
+
+
+
+for (int i = 1; i <= n; i++)
+{
+    Console.WriteLine();
+    Console.WriteLine($"Aluguel #{i}");
+    Console.Write("Nome: ");
+    string nome = Console.ReadLine();
+
+    Console.Write("Email: ");
+    string email = Console.ReadLine();
+
+    Console.Write("Quarto: ");
+    int quarto = int.Parse(Console.ReadLine());
+    vect[quarto]= new Rend(nome, email);
+}
+Console.WriteLine();
+Console.WriteLine("Quartos ocupados:");
+for (int i = 0; i < 10; i++)
+{
+    if (vect[i] != null)
+    {
+        Console.WriteLine(i + ": " + vect[i]);
+    }
+}
---
+++ OrientadaAObjetos1/array/array/Rend.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array
+{
+    internal class Rend
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+
+
+        public Rend(string nome, string email) {
+        
+        Nome = nome;
+        Email = email;
+        }
+
+
+        public override string ToString()
+        {
+            return  Nome + ", " + Email;
+
+        }
+    }
+}
