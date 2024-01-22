94c4793ff3d9d781766a8f03d424ab9256821194
Sthefany Silva <sthefany0011@gmail.com>
2024-01-22 14:24:44 -0300

    list of employees

---
+++ OrientadaAObjetos1/Lista/Lista/Funcionarios.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista
+{
+    internal class Funcionarios
+    {
+        public int Id { get;  set; }
+        public string Name { get; set; }
+        public double Salary { get; private set; }
+
+        public Funcionarios(int id, string nome, double salary)
+        {
+            Id = id;
+            Name = nome;
+            Salary = salary;
+        }
+
+
+
+        public void IncreaseSalary(double percentage)
+        {
+            Salary += Salary * percentage / 100.0;
+        }
+
+        public override string ToString()
+        {
+            return Id
+                + ", "
+                + Name
+                + ", "
+                + Salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ OrientadaAObjetos1/Lista/Lista/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Lista;
+
+
+Console.Write("How many employees will be registered? ");
+int n = int.Parse(Console.ReadLine());
+
+
+List<Funcionarios> list = new List<Funcionarios>();
+
+for (int i = 1; i < n; i++)
+{
+    Console.WriteLine($"Emplyoee #{i}:");
+    Console.Write("ID: ");
+    int id = int.Parse(Console.ReadLine());
+    Console.Write("Name: ");
+    string name = Console.ReadLine();
+    Console.Write("Salary: ");
+    double salary = double.Parse(Console.ReadLine());
+
+    list.Add(new Funcionarios(id, name, salary));
+
+    Console.WriteLine();
+}
+
+
+
+Console.Write("Enter the employee id that will have salary increase :");
+int getId = int.Parse(Console.ReadLine());
+
+Funcionarios emp = list.Find(x => x.Id == getId);
+
+if (emp != null)
+{
+    Console.Write("Enter the percentage: ");
+    double porcentage = double.Parse(Console.ReadLine());
+    emp.IncreaseSalary(porcentage);
+}
+else { Console.WriteLine("This id does not exist!"); }
+
+Console.WriteLine();
+Console.WriteLine("Updated list of employees:");
+foreach (Funcionarios obj in list)
+{
+    Console.WriteLine(obj);
+}
