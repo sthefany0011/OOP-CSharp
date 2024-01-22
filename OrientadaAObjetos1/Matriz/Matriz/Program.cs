915a5392364690643c2882a5c360cde88d35e402
Sthefany Silva <sthefany0011@gmail.com>
2024-01-22 15:59:31 -0300

    matrix exercise

---
+++ OrientadaAObjetos1/Matriz/Matriz/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Numero de matriz de ordem: ");
+int n =int.Parse(Console.ReadLine());
+
+int[,] mat =new int[n, n];
+
+for (int i = 0; i < n; i++)
+{
+    string[] values = Console.ReadLine().Split(' ');
+    for (int j = 0; j < n;)
+    {
+        mat[i,j] = int.Parse(values[j]);
+    }
+}
+Console.WriteLine("Main diagonal:");
+for (int i = 0; i < n; i++)
+{
+    Console.Write(mat[i, i] + " ");
+}
+Console.WriteLine();
+
+int count = 0;
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        if (mat[i, j] < 0)
+        {
+            count++;
+        }
+    }
+}
+Console.WriteLine("Negative numbers: " + count);
