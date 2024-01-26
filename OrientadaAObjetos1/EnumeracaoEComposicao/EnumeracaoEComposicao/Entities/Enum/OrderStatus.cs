ab91fcfb0cfd39100e53c10b0c494d8f18b71ca5
Sthefany Silva <sthefany0011@gmail.com>
2024-01-26 08:14:41 -0300

    Sistem of sales

---
+++ OrientadaAObjetos1/EnumeracaoEComposicao/EnumeracaoEComposicao/Entities/Enum/OrderStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumeracaoEComposicao.Entities.Enum
+{
+    enum OrderStatus : int
+    {
+        Pending_Payment,
+        Processing,
+        Shipped,
+        Delivered
+
+    }
+}
